17262fefe637907af23fa4e2c785d883a73e94e8
youngshehata <youngshehata10@gmail.com>
2024-08-06 21:28:41 +0300

    selecting style for whole group working

--- src/pages/Permissions/PermissionsList.jsx
+++ src/pages/Permissions/PermissionsList.jsx
@@ -25,48 +25,52 @@ export default function PermissionsList({
           <li
             className={
               permissionGroup.allSelected
-                ? `${classes.permissionGroupClass} ${classes.permissionGroupClassSelected}`
+                ? `${classes.permissionGroupClass} ${
+                    classes.permissionGroupClassSelected
+                  } ${classes[`permissionGroupClassSelected_${mode}`]}`
                 : `${classes.permissionGroupClass}`
             }
           >
-            <div className={classes.permissionGroupTopDiv}>
-              <span className={classes.permissionGroupName}>
-                {`${permissionGroup.group} ( ${permissionGroup.children.length} )`}
-              </span>
-              <img
-                onClick={toggleGroupExpanding}
-                className={classes.expandImg}
-                src="/downArrowNavy.svg"
-                alt="expand"
-              />
-              <div className={classes.wholeGroupSelectElement}>
+            <div>
+              <div className={classes.permissionGroupTopDiv}>
+                <span className={classes.permissionGroupName}>
+                  {`${permissionGroup.group} ( ${permissionGroup.children.length} )`}
+                </span>
                 <img
-                  className={classes.wholeGroupSelectImg}
-                  src="/correctWhite.svg"
-                  alt="check"
+                  onClick={toggleGroupExpanding}
+                  className={classes.expandImg}
+                  src="/downArrowNavy.svg"
+                  alt="expand"
                 />
-              </div>
-            </div>
-            <ul className={`${classes.permissionsListInsideGroup} scroll`}>
-              {permissionGroup.children.map((childPermission) => {
-                return (
-                  <Selectable
-                    key={childPermission.id}
-                    id={childPermission.id}
-                    handleSelection={handlePermissionSelection}
-                    cssClass={classes.permissionLi}
-                    backgroundColor={mode == "grant" ? "green" : "darkred"}
-                    color={childPermission.selected ? "#fff" : "#000"}
-                    isSelected={childPermission.selected}
-                    label={
-                      language == "ar"
-                        ? childPermission.labelAr
-                        : childPermission.labelEn
-                    }
+                <div className={classes.wholeGroupSelectElement}>
+                  <img
+                    className={classes.wholeGroupSelectImg}
+                    src="/correctWhite.svg"
+                    alt="check"
                   />
-                );
-              })}
-            </ul>
+                </div>
+              </div>
+              <ul className={`${classes.permissionsListInsideGroup} scroll`}>
+                {permissionGroup.children.map((childPermission) => {
+                  return (
+                    <Selectable
+                      key={childPermission.id}
+                      id={childPermission.id}
+                      handleSelection={handlePermissionSelection}
+                      cssClass={classes.permissionLi}
+                      backgroundColor={mode == "grant" ? "green" : "darkred"}
+                      color={childPermission.selected ? "#fff" : "#000"}
+                      isSelected={childPermission.selected}
+                      label={
+                        language == "ar"
+                          ? childPermission.labelAr
+                          : childPermission.labelEn
+                      }
+                    />
+                  );
+                })}
+              </ul>
+            </div>
           </li>
         );
       })}
